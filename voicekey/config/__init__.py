bd7b596dc07b9140c1313a6551a17adc04bd7981
ihmorol <ekramulhasane69@gmail.com>
2026-02-20 01:57:47 +0600

    chore(config): export config APIs and align dev dependencies

--- voicekey/config/__init__.py
+++ voicekey/config/__init__.py
@@ -1,1 +1,31 @@
-"""Configuration management."""
+"""Configuration management exports."""
+
+from voicekey.config.manager import (
+    ConfigError,
+    ConfigLoadResult,
+    backup_config,
+    load_config,
+    resolve_config_path,
+    save_config,
+)
+from voicekey.config.schema import (
+    CONFIG_VERSION,
+    VoiceKeyConfig,
+    default_config,
+    serialize_config,
+    validate_with_fallback,
+)
+
+__all__ = [
+    "CONFIG_VERSION",
+    "ConfigError",
+    "ConfigLoadResult",
+    "VoiceKeyConfig",
+    "backup_config",
+    "default_config",
+    "load_config",
+    "resolve_config_path",
+    "save_config",
+    "serialize_config",
+    "validate_with_fallback",
+]
