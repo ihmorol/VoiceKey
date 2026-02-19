4347ce605aa3cbedfb200813cc6979c15a9f4250
ihmorol <ekramulhasane69@gmail.com>
2026-02-20 00:36:17 +0600

    refactor: export parser and state machine public APIs

--- voicekey/app/__init__.py
+++ voicekey/app/__init__.py
@@ -1,1 +1,21 @@
 """VoiceKey application core."""
+
+from voicekey.app.state_machine import (
+    AppEvent,
+    AppState,
+    InvalidTransitionError,
+    ListeningMode,
+    ModeHooks,
+    TransitionResult,
+    VoiceKeyStateMachine,
+)
+
+__all__ = [
+    "AppEvent",
+    "AppState",
+    "InvalidTransitionError",
+    "ListeningMode",
+    "ModeHooks",
+    "TransitionResult",
+    "VoiceKeyStateMachine",
+]
--- voicekey/commands/__init__.py
+++ voicekey/commands/__init__.py
@@ -1,1 +1,23 @@
 """Command parsing and registry."""
+
+from voicekey.commands.builtins import create_builtin_registry
+from voicekey.commands.parser import CommandParser, ParseKind, ParseResult
+from voicekey.commands.registry import (
+    CommandChannel,
+    CommandDefinition,
+    CommandRegistry,
+    FeatureGate,
+    normalize_phrase,
+)
+
+__all__ = [
+    "CommandChannel",
+    "CommandDefinition",
+    "CommandParser",
+    "CommandRegistry",
+    "FeatureGate",
+    "ParseKind",
+    "ParseResult",
+    "create_builtin_registry",
+    "normalize_phrase",
+]
